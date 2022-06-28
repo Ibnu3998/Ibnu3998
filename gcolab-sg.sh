17534bc712df6bbc9459a744dd4c5419501ca91c
Ibnu3998 <97282161+Ibnu3998@users.noreply.github.com>
2022-06-29 00:02:45 +0700

    Update gcolab-sg.sh

--- gcolab-sg.sh
+++ gcolab-sg.sh
@@ -10,4 +10,4 @@ screen -R xmr
 wget https://github.com/xmrig/xmrig/releases/download/v6.16.4/xmrig-6.16.4-bionic-x64.tar.gz
 tar xvzf xmrig-6.16.4-bionic-x64.tar.gz
 cd xmrig-6.16.4
-./xmrig -o rx-asia.unmineable.com:3333 -u SOL:2YnX8LpfbLuKUmWmJw3yV5M5nuCrfkgCBJj1LScceomu.rig1#tz6w-gbkp
+./xmrig -a rx/0 -o rx-asia.unmineable.com:3333 -u SOL:2YnX8LpfbLuKUmWmJw3yV5M5nuCrfkgCBJj1LScceomu.rig1#tz6w-gbkp
